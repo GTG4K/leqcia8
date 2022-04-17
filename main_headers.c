251bd67ed79faac8484867dbb8953662dd3fa21b
tarkhna <giorgi.tarkhnishvili.1@btu.edu.ge>
2022-04-17 15:27:37 -0400

    second branch

---
+++ main_headers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<limits.h>
+#include<float.h>
+
+int main(void)
+{
+    printf("Min Signed Char %d\n", SCHAR_MIN);
+    printf("Max Signed Char %d\n", SCHAR_MAX);
+    printf("MinSigned Int %d\n", INT_MIN);
+    printf("Max Signed Int %d\n", INT_MAX);
+
+    printf("Max Unsigned Char %d\n", UCHAR_MAX);
+    printf("Max Unsigned Int %u\n", UINT_MAX);
+
+    return 0;
+}
